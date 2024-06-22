bfc1e8da016cbc2b7bd903ab30153fb48590fbe6
rohit1203 <jadhavsrohit7@gmail.com>
2024-06-22 17:35:52 +0530

    use of DBRef

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -3,6 +3,7 @@ package net.engineeringdigest.journalApp.entity;
 import com.mongodb.client.model.Collation;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -16,6 +17,7 @@ import java.util.Date;
 public class JournalEntry {
     @Id
     private ObjectId id;
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -1,22 +1,38 @@
 package net.engineeringdigest.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+
+
 @Component
+@Slf4j
 public class JournalEntryService {
 
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
     public void saveEntry(JournalEntry journalEntry){
-        journalEntryRepository.save(journalEntry);
+
+        try{
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepository.save(journalEntry);
+        }catch (Exception e){
+            //throw new RuntimeException("Error", e);
+            log.error("Message", e);
+        }
     }
 
     public List<JournalEntry> getAll(){
