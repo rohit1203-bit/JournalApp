d997753b5f33eaf57762987e7d0a74f40376471a
rohit1203 <jadhavsrohit7@gmail.com>
2024-07-08 13:39:09 +0530

    Gmail SMTP, sending mail with the help of JavaMailSender

---
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,21 @@
+package net.engineeringdigest.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail(){
+        emailService.sendEmail(
+                "rathodkaran039332@gmail.com",
+                "Testing Java Mail Sender",
+                "How are you?");
+
+    }
+}
